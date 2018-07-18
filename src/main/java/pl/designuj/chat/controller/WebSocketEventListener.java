b2825949e2d7065c722c0929de0b1c68e1b09647
designuj <designuj@fejm.pl>
2018-07-18 13:13:36 +0200

    First Commit

---
+++ src/main/java/pl/designuj/chat/controller/WebSocketEventListener.java
@@ -0,0 +1,40 @@
+package pl.designuj.chat.controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import pl.designuj.chat.model.ChatMessage;
+
+@Component
+public class WebSocketEventListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        LOGGER.info("Received new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            LOGGER.info("User disconnected: " + username);
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
